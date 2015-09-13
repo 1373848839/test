8bd3a23609c29eeb3958e4ee89bbfaf2dcf229ad
顾谋 <guchuan_cc@163.com>
2015-09-13 17:14:00 +0800

    one.php

--- one.php
+++ one.php
@@ -19,4 +19,22 @@
 	}
 }
 var_dump(my_scandir("C:\wamp\www\gc\ms"));
+
+echo "<hr />";
+function getExt($url){
+
+   $arr = parse_url($url);
+//var_dump($arr);
+    $file = basename($arr['path']);
+  //  var_dump($file);
+
+    $ext = explode(“.”,$file);
+    //var_dump($ext);
+
+    return $ext[0];
+
+}
+$url='http://www.sina.com.cn/abc/de/fg.php?id=1';
+echo (getExt($url));
+
 ?>
