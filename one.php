27471a79f2942798b4dbc75be08b58a1ab3036f7
顾谋 <guchuan_cc@163.com>
2015-09-13 17:07:57 +0800

    001

--- one.php
+++ one.php
@@ -1,3 +1,22 @@
 <?php 
 	echo "1111111111111";
+
+
+	function my_scandir($dir){ 
+	$files=array(); 
+	if($handle=opendir($dir)){ 
+		while(($file=readdir($handle))!==false){ 
+			if($file!="." && $file !=".."){ 
+				if(is_dir($dir."/".$file)){ 
+					$files[$file]= scandir($dir."/".$file);
+				}else{ 
+					$files[]=$dir."/".$file; 
+				}
+			}
+		}
+		closedir($handle);
+		return $files;
+	}
+}
+var_dump(my_scandir("C:\wamp\www\gc\ms"));
 ?>
